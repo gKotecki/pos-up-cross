807e6c47d4857cdf7cdf9f905fc0777d5f177d69
gkotecki <gabrielkotecki@gmail.com>
2018-06-30 13:39:06 -0300

    models e services

---
+++ UberSeries/UberSeries/UberSeries/Services/ISerieService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UberSeries.Models;
+
+namespace UberSeries.Services
+{
+    public interface ISerieService
+    {
+        Task<IEnumerable<SerieResponse>> GetSeriesAsync();
+    }
+}
---
+++ UberSeries/UberSeries/UberSeries/Services/SerieService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UberSeries.Models;
+
+namespace UberSeries.Services
+{
+    public class SerieService : ISerieService
+    {
+        public Task<IEnumerable<SerieResponse>> GetSeriesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
