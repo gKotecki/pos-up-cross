e3d69c20f4cc0310e180cc43ee36655e362e5125
gkotecki <gabrielkotecki@gmail.com>
2018-06-29 22:26:30 -0300

    correçao de namespace

--- UberSeries/UberSeries/UberSeries/Services/Navigation/Navigation.cs
+++ UberSeries/UberSeries/UberSeries/Services/Navigation/Navigation.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using UberSeries.ViewModel;
 using UberSeries.ViewModel.Base;
+using UberSeries.Views;
 using Xamarin.Forms;
 
 namespace UberSeries.Services
